abc532d8290026a7bc21982db9f44d474854d6ff
ashu8tosh <ashutosh3089@gmail.com>
2019-10-23 14:57:04 +0530

    PalTrackerApplication class changes to return an instance of JdbcTimeEntryRepository

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -3,17 +3,21 @@ package io.pivotal.pal.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
 
+    @Bean
+    public JdbcTimeEntryRepository getTimeEntryRepository(DataSource ds){
+        return new JdbcTimeEntryRepository(ds);
+    }
 
+   /* public TimeEntryRepository getTimeEntryRepository(){
+        return new InMemoryTimeEntryRepository();
+    } */
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
-    }
-    @Bean
-    public TimeEntryRepository getTimeEntryRepository(){
-        return new InMemoryTimeEntryRepository();
     }
 }
